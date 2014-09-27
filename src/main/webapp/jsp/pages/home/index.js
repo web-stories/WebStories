787c341ecc8a549d399fba08f260ee7f09b53682
Fagner Brack <github@fagnermartins.com>
2014-09-27 14:12:02 -0300

    Should not close modal when clicking in the backdrop

--- src/main/webapp/jsp/pages/home/index.js
+++ src/main/webapp/jsp/pages/home/index.js
@@ -1,7 +1,8 @@
 require( ["jquery", "bootstrap", "bootstrap.wizard"], function( $ ) {
 	var nostory = $( "#meta" ).data( "nostory" ) === true;
 	var modal = $( "#new-story-wizard-modal" ).modal({
-		show: nostory
+		show: nostory,
+		backdrop: "static"
 	});
 	$( "#new-story-wizard" ).wizard({
 		finish: function() {
