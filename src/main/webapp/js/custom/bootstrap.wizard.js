31066fe57de4d192fcfc81c43a7648ed02376297
Fagner Brack <github@fagnermartins.com>
2014-09-27 08:21:56 -0300

    Enhances the methods structure for a better understanding

--- src/main/webapp/js/custom/bootstrap.wizard.js
+++ src/main/webapp/js/custom/bootstrap.wizard.js
@@ -3,8 +3,49 @@ define( ["jquery", "bootstrap", "jquery.ui.widget"], function( $ ) {
 	
 	$.widget( "ws.wizard", {
 		_create: function() {
+			this._refresh();
+		},
+		_activatePane: function( oldStep, activeStep ) {
+			var oldPane = this.element.find( ".wizard-pane" ).eq( oldStep );
+			var activePane = this.element.find( ".wizard-pane" ).eq( activeStep );
+			
+			function activate() {
+				activePane[ 0 ].offsetWidth; // reflow for transition
+				activePane.addClass( "in" );
+			}
+			
+			oldPane
+				.removeClass( "active in" );
+			activePane
+				.addClass( "active" )
+				.one( "bsTransitionEnd", activate )
+				.emulateTransitionEnd( 150 );
+				
+			this._currentStep = activeStep;
+		},
+		_activatePrevStep: function( prev ) {
+			this.element.find( ".wizard-steps > li.active" )
+				.removeClass( "active" );
+			this.element.find( ".wizard-steps > li" ).eq( prev )
+				.removeClass( "complete" )
+				.addClass( "active" );
+		},
+		_activateNextStep: function( next ) {
+			this.element.find( ".wizard-steps > li.active" )
+				.removeClass( "active" )
+				.addClass( "complete" );
+			this.element.find( ".wizard-steps > li" ).eq( next )
+				.addClass( "active" );
+		},
+		_refresh: function() {
 			this._prevControl= this.element.find( ".wizard-control-prev" );
 			this._nextControl = this.element.find( ".wizard-control-next" );
+			this._currentStep = this.element.find( ".wizard-steps .active" ).index();
+			this._stepsNumber = this.element.find( ".wizard-steps li" ).length;
+			
+			this._off( this._prevControl, "click" );
+			this._off( this._nextControl, "click" );
+			
 			this._on( this._prevControl, {
 				"click": this.prev
 			});
@@ -12,65 +53,21 @@ define( ["jquery", "bootstrap", "jquery.ui.widget"], function( $ ) {
 				"click": this.next
 			});
 		},
-		_getCurrentStep: function() {
-			return this.element.find( ".wizard-steps .active" ).index();
-		},
-		_getStepsNumber: function() {
-			return this.element.find( ".wizard-pane" ).length;
-		},
-		_changeStep: function( oldIndex, activeIndex ) {
-			var activeStep, activePane, oldPane, transition;
-			transition = $.support.transition;
-			activeStep = this.element.find( ".wizard-steps li" ).eq( activeIndex );
-			activePane = this.element.find( ".wizard-pane" ).eq( activeIndex );
-			oldPane = this.element.find( ".wizard-pane" ).eq( oldIndex );
-			
-			// TODO deal with fast step changes
-			activeStep
-				.prevAll()
-				.removeClass( "active" )
-				.addClass( "complete" );
-			activeStep
-				.nextAll()
-				.removeClass( "active complete" );
-			activeStep
-				.removeClass( "complete" )
-				.addClass( "active" );
-				
-			function change() {
-				if ( transition ) {
-					activePane[ 0 ].offsetWidth; // reflow for transition
-					activePane.addClass( "in" );
-				} else {
-					activePane.removeClass( "fade" );
-				}
-			}
-			
-			oldPane
-				.removeClass( "active" );
-			activePane
-				.removeClass( "in" )
-				.addClass( "active" );
-			
-			if ( transition ) {
-				activePane
-					.one( "bsTransitionEnd", change )
-					.emulateTransitionEnd( 150 );
-			} else {
-				change();
-			}
-		},
 		prev: function() {
-			var step = this._getCurrentStep();
-			if ( step ) {
-				this._changeStep( step, step - 1 );
+			var current = this._currentStep;
+			var prev = current - 1;
+			if ( current ) {
+				this._activatePane( current, prev );
+				this._activatePrevStep( prev );
 			}
 		},
 		next: function() {
-			var stepsNumber = this._getStepsNumber();
-			var step = this._getCurrentStep();
-			if ( step < stepsNumber - 1 ) {
-				this._changeStep( step, step + 1 );
+			var last = this._stepsNumber - 1;
+			var current = this._currentStep;
+			var next = current + 1;
+			if ( current < last ) {
+				this._activatePane( current, next );
+				this._activateNextStep( next );
 			}
 		}
 	});
