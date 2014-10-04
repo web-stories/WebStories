29291b396261068cf78a9d17098d8dfaedee3990
Fagner Brack <github@fagnermartins.com>
2014-10-03 22:45:32 -0300

    Converts editor into a local component

--- src/main/webapp/jsp/pages/home/projects/index.js
+++ src/main/webapp/jsp/pages/home/projects/index.js
@@ -1,28 +1,44 @@
-require( ["jquery", "bootstrap"], function( $ ) {
-	var offset = $( ".header-navbar" ).outerHeight( true );
-	var thumbsNav = ".editor-chapter-thumbs";
-	function click( event ) {
-		var chapterId = $( this ).attr( "href" );
-		event.preventDefault();
-		jumpTo( chapterId );
-	}
-	function jumpTo( chapterId ) {
-		$( "html, body" ).animate({
-			scrollTop: $( chapterId ).offset().top - offset
-		}, "fast" );
-	}
-	$( "body" ).scrollspy({
-		target: thumbsNav,
-		offset: offset + 1
-	});
-	$( thumbsNav ).find( "a" )
-		.click( click );
-});
-
-require( ["jquery", "bootstrap"], function( $ ) {
-	$( ".editor-chapter-thumbs" ).affix({
-		offset: {
-			top: 168
+require( ["jquery", "jquery.ui.widget", "bootstrap"], function( $ ) {
+	$.widget( "ws.editor", {
+		options: {
+			chaptersOffset: 0
+		},
+		_create: function() {
+			this._cacheElements();
+			this._setupEvents();
+			this._setupComponents();
+		},
+		_cacheElements: function() {
+			this._chapters = this.element.find( ".editor-chapter-thumbs" );
+		},
+		_setupEvents: function() {
+			this._on( this._chapters, {
+				"click a": function( event ) {
+					var chapterId = $( event.currentTarget ).attr( "href" );
+					event.preventDefault();
+					this._switchChapter( chapterId );
+				}
+			});
+		},
+		_setupComponents: function() {
+			this._chapters.affix({
+				offset: {
+					top: 168 // TODO calculate
+				}
+			});
+			$( "body" ).scrollspy({
+				target: this.options.spyTarget,
+				offset: this.options.chaptersOffset + 1
+			});
+		},
+		_switchChapter: function( chapterId ) {
+			$( "html, body" ).animate({
+				scrollTop: $( chapterId ).offset().top - this.options.chaptersOffset
+			}, "fast" );
 		}
+	});
+	$( ".editor" ).editor({
+		chaptersOffset: $( ".header-navbar" ).outerHeight( true ),
+		spyTarget: ".editor .editor-chapter-thumbs"
 	});
 });
