50215e2d58441dc8505938837aeb283426df9d2e
Fagner Brack <github@fagnermartins.com>
2014-10-14 08:44:03 -0300

    Add jscs for grunt tasks declarations

--- grunt/options/jscs.js
+++ grunt/options/jscs.js
@@ -2,7 +2,7 @@ module.exports = function( grunt ) {
 	return {
 		files: [
 			"Gruntfile.js",
-			//"grunt/**/*.js",
+			"grunt/**/*.js"
 			//"src/main/webapp/js/custom/**/*.js",
 			//"src/main/webapp/jsp/pages/**/*.js"
 		]
