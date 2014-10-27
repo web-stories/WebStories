00d5a963f46d03f211e3a20450c8c4a019ae9af1
Fagner Brack <github@fagnermartins.com>
2014-10-27 17:41:33 -0200

    Remove the need to calculate the slide height
    Let's think about that later.

--- src/main/webapp/js/custom/jquery.ws.story.js
+++ src/main/webapp/js/custom/jquery.ws.story.js
@@ -19,26 +19,9 @@ define( ["jquery", "jquery.ui.widget", "impress"], function( $ ) {
 								.attr( "data-x", x );
 							x += docWidth + 300;
 						});
-				},
-				slideHeight: function() {
-					var doc = $( document );
-					var footer = element.find( ".story-footer" );
-					var content = element.find( ".story-content" );
-					var data = {
-						offset: element.offset().top,
-						document: doc.height(),
-						footer: footer.height()
-					};
-					
-					console.log( "---- slideHeight ----" );
-					console.log( data );
-					console.log( "---- slideHeight ----" );
-					
-					content.height( data.document - data.offset - data.footer );
 				}
 			};
 			refresh.slideData();
-			refresh.slideHeight();
 		}
 	});
 });
