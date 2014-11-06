cecc986e8722c4f482910bb00a38533f74bcdd79
Fagner Brack <github@fagnermartins.com>
2014-11-06 07:59:53 -0200

    Enable possibility to disable impress via URL for debug purposes

--- src/main/webapp/jsp/pages/view/stories/index.js
+++ src/main/webapp/jsp/pages/view/stories/index.js
@@ -1,5 +1,16 @@
 require( ["jquery", "jquery.ws.story"], function( $ ) {
 	"use strict";
+	
+	var isDebug = function() {
+		return window.location.href.indexOf( "?debug" ) !== -1;
+	};
+	
+	if ( isDebug() ) {
+		$( "body" )
+			.removeClass( "impress-supported" )
+			.addClass( "impress-not-supported" );
+	}
+	
 	if ( $( ".impress-supported" ).length ) {
 		$( ".story" ).story();
 	}
