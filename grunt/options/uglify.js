a2e540f42d14d358d51a3a0ca29e9c390daded48
Fagner Brack <github@fagnermartins.com>
2014-11-19 18:14:55 -0200

    Optimize resources
    Closes gh-13. Closes gh-14. Closes gh-22.

---
+++ grunt/options/uglify.js
@@ -0,0 +1,15 @@
+module.exports = function( grunt ) {
+	"use strict";
+	return {
+		options: {
+			sourceMap: true,
+			sourceMapIncludeSources: true
+		},
+		files: {
+			expand: true,
+			cwd: "src/main/webapp/static/js/",
+			src: ["*.js"],
+			dest: "src/main/webapp/static/js/"
+		}
+	};
+};
