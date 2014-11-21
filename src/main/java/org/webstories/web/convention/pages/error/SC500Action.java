4ce70435856c37416e009bc657eb678cf4fe072c
Fagner Brack <github@fagnermartins.com>
2014-11-20 22:03:33 -0200

    the annotation is required here to make EJB work (not in superclass)

--- src/main/java/org/webstories/web/convention/pages/error/SC500Action.java
+++ src/main/java/org/webstories/web/convention/pages/error/SC500Action.java
@@ -1,10 +1,12 @@
 package org.webstories.web.convention.pages.error;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.webstories.web.util.servlet.ErrorServlet;
 
+@WebServlet
 public class SC500Action extends ErrorServlet {
 	private static final long serialVersionUID = 1;
 	@Override
