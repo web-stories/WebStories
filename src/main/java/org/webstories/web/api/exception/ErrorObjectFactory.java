96231954c21c55513d234f7a13837971b720b2c1
Fagner Brack <github@fagnermartins.com>
2014-12-01 22:05:48 -0200

    Ensure stability for the ErrorObjectFactory#create behavior
    The message chosen to be presented in the view represents the exception
    message closest to the root cause. If the root cause itself doesn't have
    a message, get the next one bubling up the causes tree.

--- src/main/java/org/webstories/web/api/exception/ErrorObjectFactory.java
+++ src/main/java/org/webstories/web/api/exception/ErrorObjectFactory.java
@@ -2,14 +2,18 @@ package org.webstories.web.api.exception;
 
 public class ErrorObjectFactory {
 	public static ErrorObject create( Throwable e ) {
-		Throwable root = e;
+		Throwable currentCause = e;
+		String message = e.getMessage();
 		
-		while ( root.getCause() != null ) {
-			root = root.getCause();
+		// Iterate over all causes to find the closest message to the root cause
+		while ( currentCause.getCause() != null ) {
+			currentCause = currentCause.getCause();
+			if ( currentCause.getMessage() != null ) {
+				message = currentCause.getMessage();
+			}
 		}
 		
-		String message = root.getMessage();
-		
+		// No message was found...
 		if ( message == null ) {
 			message = "Unknown error";
 		}
