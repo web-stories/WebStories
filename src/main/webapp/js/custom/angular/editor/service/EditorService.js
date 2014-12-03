fce5db2087c662a5d74a018816a9b05264a7bf08
Fagner Brack <github@fagnermartins.com>
2014-12-03 08:51:22 -0200

    Adjust method to remove a section

--- src/main/webapp/js/custom/angular/editor/service/EditorService.js
+++ src/main/webapp/js/custom/angular/editor/service/EditorService.js
@@ -56,7 +56,10 @@ define(function() {
 			.$promise
 				.then(
 					function resolve() {
-						refresh( storyId );
+						refresh( storyId )
+							.then(function( editor ) {
+								$rootScope.$broadcast( "editor:updated", editor );
+							});
 					}
 				);
 		};
