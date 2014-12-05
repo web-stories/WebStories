a0adb3bed0f78605e7f67218c2ca967dd1593710
Fagner Brack <github@fagnermartins.com>
2014-12-05 19:58:35 -0200

    Rename to "FileResource"
    To be able to use ResourceNotFoundException name for application
    resources read from database

--- src/main/java/org/webstories/core/resources/FileResourceReader.java
+++ src/main/java/org/webstories/core/resources/FileResourceReader.java
@@ -5,16 +5,16 @@ import java.io.InputStream;
 import javax.ejb.Stateless;
 
 @Stateless
-public class ResourceReader {
+public class FileResourceReader {
 	/**
 	 * Get an application resource from the classpath.
 	 * @return The stream representing the resource. This stream should be closed.
 	 */
-	public InputStream getResourceStream( String fileName ) throws ResourceNotFoundException {
+	public InputStream getResourceStream( String fileName ) throws FileResourceNotFoundException {
 		ClassLoader currentClassloader = this.getClass().getClassLoader();
 		InputStream input = currentClassloader.getResourceAsStream( fileName );
 		if ( input == null ) {
-			throw new ResourceNotFoundException();
+			throw new FileResourceNotFoundException();
 		}
 		return input;
 	}
