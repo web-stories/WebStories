cf250139193ab0f3d7b616bdc2dee8a933f31393
Fagner Brack <github@fagnermartins.com>
2014-12-18 15:03:33 -0200

    Check rule for story viewer and previewer access

--- src/main/java/org/webstories/core/story/facade/StoryViewerReader.java
+++ src/main/java/org/webstories/core/story/facade/StoryViewerReader.java
@@ -1,20 +1,33 @@
 package org.webstories.core.story.facade;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.webstories.core.auth.Logged;
+import org.webstories.core.security.AccessDeniedException;
+import org.webstories.core.security.story.StoryOwnerSecurity;
+import org.webstories.core.security.story.StoryRead;
+import org.webstories.dao.story.StoryEntity;
 
 @Stateless
 public class StoryViewerReader implements LocalStoryViewerReader {
+	@PersistenceContext
+	EntityManager entityManager;
+	
 	@Override
 	public boolean isPubliclyViewable( long idStory ) {
-		// TODO implement
-		return false;
+		StoryEntity story = entityManager.find( StoryEntity.class, idStory );
+		return story != null;
 	}
 	@Override
 	public boolean isPreviewable( long idStory, Logged logged ) {
-		// TODO implement
-		return false;
+		StoryOwnerSecurity security = new StoryOwnerSecurity( logged );
+		try {
+			security.readPrivileged( new StoryRead.DefaultRead( idStory, entityManager ) );
+			return true;
+		} catch ( AccessDeniedException e ) {
+			return false;
+		}
 	}
-	
 }
