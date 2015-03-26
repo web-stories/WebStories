417cfdaa4f14fd444fa1f18156f648d593cb6094
Fagner Brack <github@fagnermartins.com>
2015-03-26 06:58:40 -0300

    Add a title to public pages that do not have one yet

--- src/main/java/org/webstories/web/convention/pages/AboutAction.java
+++ src/main/java/org/webstories/web/convention/pages/AboutAction.java
@@ -4,6 +4,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.webstories.web.IndividualPageSEO;
 import org.webstories.web.util.servlet.BaseServlet;
 
 import com.fagnerbrack.servlet.convention.ConventionServlet;
@@ -13,5 +14,11 @@ import com.fagnerbrack.servlet.convention.ConventionServlet;
 public class AboutAction extends BaseServlet {
 	private static final long serialVersionUID = 1;
 	@Override
-	protected void doGet( HttpServletRequest request, HttpServletResponse response ) {};
+	protected void doGet( HttpServletRequest request, HttpServletResponse response ) {
+		request.setAttribute( "seo", new IndividualPageSEO
+			.Builder()
+				.title( "Sobre Web Stories" )
+			.build()
+		);
+	};
 }
